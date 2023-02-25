dc058783e9555314254c626f2caa5b9441fc0df5
Jinil Panawala <Owner@Jinils-MacBook-Pro.local>
2023-02-25 02:21:52 -0500

    changed home text, uploaded new resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-                    I'm a Computer Science Student
+                    Hi, I'm Jinil Panawala
                 </h2>
                 <p className='text-gray-500 py-4 max-w-md'>
                     Currently studying Computer Science 
-                    at the University of Waterloo
+                    at the University of Waterloo.
                     
                 </p>
                 <div>
