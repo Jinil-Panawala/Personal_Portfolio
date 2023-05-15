ed0e90d92af3d1bcff99a890d61cbdb1273d361f
Jinil Panawala <Owner@Jinils-MacBook-Pro.local>
2023-05-15 15:57:10 -0400

    test 2

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -96,7 +96,7 @@ const Experience = () => {
             
             <div>
                 <p className='text-4xl font-bold border-b-4 border-gray-500 inline'>Experience</p>
-                <p className='py-6'>Throuh schooling, courses, and projects, I have experience in the following languages, frameworks, and technologies:</p>
+                <p className='py-6'>Through schooling, courses, and projects, I have experience in the following languages, frameworks, and technologies:</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
